8a9aa8f543b138a5cbd8bc3d83e2680fde353366
Joffrey Bion <joffrey.bion@amadeus.com>
2017-08-30 18:07:25 +0200

    Rework InterfaceSubtypeMapper to handle all abstract classes

--- livedoc-core/src/main/java/org/hildan/livedoc/core/scanner/types/mappers/TypeMapper.java
+++ livedoc-core/src/main/java/org/hildan/livedoc/core/scanner/types/mappers/TypeMapper.java
@@ -1,8 +1,8 @@
 package org.hildan.livedoc.core.scanner.types.mappers;
 
-import java.util.Collection;
+import java.util.Set;
 import java.util.function.Function;
 
 @FunctionalInterface
-public interface TypeMapper extends Function<Class<?>, Collection<Class<?>>> {
+public interface TypeMapper extends Function<Class<?>, Set<Class<?>>> {
 }
