dc0c0eda348a8b7ab904c4883500c99d702652eb
Joffrey BION <joffrey.bion@gmail.com>
2017-09-26 23:12:48 +0200

    Handle doc fetch error

--- livedoc-ui/src/redux/reducer.js
+++ livedoc-ui/src/redux/reducer.js
@@ -1,20 +1,24 @@
 // @flow
 
-import {fromJS} from 'immutable';
+import {Record} from 'immutable';
 import {State} from '../models/state';
+import {Livedoc} from '../models/livedoc';
 
 export const types = {
   FETCH_DOC: 'DOC/FETCH',
   DOC_FETCHED: 'DOC/FETCHED',
+  DOC_FETCH_ERROR: 'DOC/FETCH_ERROR',
 };
 
 export type Action =
         | { type: 'DOC/FETCH', url: string }
-        | { type: 'DOC/FETCHED', jsonDoc: string };
+        | { type: 'DOC/FETCHED', jsonDoc: string }
+        | { type: 'DOC/FETCH_ERROR', error: Error };
 
 export const actions = {
   fetchDoc: (url: string) => ({ type: types.FETCH_DOC, url }),
-  updateDoc: (jsonDoc: string) => ({ type: types.DOC_FETCHED, jsonDoc }),
+  updateDoc: (jsonDoc: any) => ({ type: types.DOC_FETCHED, jsonDoc }),
+  fetchError: (error: Error) => ({ type: types.DOC_FETCH_ERROR, error }),
 };
 
 export default (state: State = new State(), action: any) => {
@@ -29,7 +33,13 @@ export default (state: State = new State(), action: any) => {
       return {
         loading: false,
         url: state.url,
-        livedoc: fromJS(JSON.parse(action.jsonDoc))
+        livedoc: new Livedoc(action.jsonDoc)
+      };
+    case types.DOC_FETCH_ERROR:
+      return {
+        loading: false,
+        url: state.url,
+        livedoc: null
       };
     default:
       return state;
--- livedoc-ui/src/sagas/watchFetchDoc.js
+++ livedoc-ui/src/sagas/watchFetchDoc.js
@@ -1,16 +1,17 @@
-import { call, put, takeLatest } from 'redux-saga/effects'
+import { apply, call, put, takeLatest } from 'redux-saga/effects'
 import type {Action} from '../redux/reducer';
 import {actions, types} from '../redux/reducer';
 
 function* fetchDoc(action: Action): * {
   try {
     console.log('Fetching documentation at', action.url);
-    const data = yield call(fetch, action.url);
+    const response = yield call(fetch, action.url, {mode: 'cors'});
+    const data =  yield apply(response, response.json);
     console.log('Fetched documentation:', data);
     yield put(actions.updateDoc(data));
   } catch (error) {
     console.error('Could not fetch documentation', error);
-    yield put({type: "FETCH_FAILED", error});
+    yield put(actions.fetchError(error));
   }
 }
 
