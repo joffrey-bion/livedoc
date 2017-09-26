82b0de45767ceb43db7801ab714ce887460616be
Joffrey BION <joffrey.bion@gmail.com>
2017-09-26 23:49:38 +0200

    Add version and basePath fields

--- livedoc-ui/src/models/livedoc.js
+++ livedoc-ui/src/models/livedoc.js
@@ -3,11 +3,15 @@ import { Map, Record } from 'immutable';
 import {Api} from './api';
 
 export type LivedocShape = {
+  version: string,
+  basePath: string,
   apis: Map<String, Api>
 }
 export type LivedocType = Record<LivedocShape>;
 
 const LivedocRecord: LivedocType = Record({
+  version: '',
+  basePath: '',
   apis: Map()
 });
 
