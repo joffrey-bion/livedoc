72197bd066355e751ae2d2ef86462fca6e1175eb
Joffrey BION <joffrey.bion@gmail.com>
2017-10-18 02:37:33 +0200

    Add links on types

--- livedoc-ui/src/components/doc/content/common/TypeInfo.jsx
+++ livedoc-ui/src/components/doc/content/common/TypeInfo.jsx
@@ -1,13 +1,36 @@
 // @flow
 import * as React from 'react';
-import type { LivedocType } from '../../../../model/livedoc';
+import { Link } from 'react-router-dom';
+import type { LivedocType, TypeElement } from '../../../../model/livedoc';
 
 export type TypeInfoProps = {
   type: LivedocType,
   required: ?boolean,
 }
 
+type TypeElementProps = {
+  element: TypeElement,
+}
+
+const TypeElementLink = ({element}: TypeElementProps) => {
+  const text = <code>{element.text}</code>;
+
+  if (element.link) {
+    return <Link to={'/types/' + element.link}>{text}</Link>
+  } else {
+    return text;
+  }
+};
+
 export const TypeInfo = (props: TypeInfoProps) => {
   const optionalMark = props.required === false ? '?' : '';
-  return <a href={"/"}><code>{props.type.oneLineText + optionalMark}</code></a>;
+
+  // this if handles old versions of Livedoc
+  // TODO remove when unnecessary
+  if (props.type.typeElements) {
+    return props.type.typeElements.map(e => <TypeElementLink key={e.text} element={e}/>);
+  } else {
+    return <code>{props.type.oneLineText + optionalMark}</code>;
+  }
 };
+
