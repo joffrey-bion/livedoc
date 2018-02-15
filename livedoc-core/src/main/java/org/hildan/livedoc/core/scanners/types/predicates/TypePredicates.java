20889fa2989f619dd0ab91d30be54c73e97fe446
Joffrey BION <joffrey.bion@gmail.com>
2018-02-15 21:45:25 +0100

    Remove unused import

--- livedoc-core/src/main/java/org/hildan/livedoc/core/scanners/types/predicates/TypePredicates.java
+++ livedoc-core/src/main/java/org/hildan/livedoc/core/scanners/types/predicates/TypePredicates.java
@@ -2,7 +2,6 @@ package org.hildan.livedoc.core.scanners.types.predicates;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 
