d11acab0a029151e9d1e1b3d03bdaa5551cb8cba
Joffrey BION <joffrey.bion@gmail.com>
2018-03-01 02:58:41 +0100

    Add display of the field property format when present

--- livedoc-ui-webjar/src/components/doc/types/complex/TypeFieldRow.jsx
+++ livedoc-ui-webjar/src/components/doc/types/complex/TypeFieldRow.jsx
@@ -9,10 +9,26 @@ export type TypeFieldRowProps = {
 
 export const TypeFieldRow = (props: TypeFieldRowProps) => {
   const field: ApiObjectFieldDoc = props.field;
-
+  const desc = field.description && <p>{field.description}</p>;
   return <tr>
     <td><code>{field.name}</code></td>
     <td><TypeRef type={field.type}/></td>
-    <td>{field.description}</td>
+    <td>{desc}<Format constraints={field.format}/></td>
   </tr>;
+};
+
+type FormatProps = {
+  constraints: Array<string>,
+}
+
+const Format = ({constraints}: FormatProps) => {
+  if (constraints.length === 0) {
+    return null;
+  }
+  if (constraints.length === 1) {
+    return [<h6>Format:</h6>, constraints[0]];
+  }
+  const items = constraints.map((c, index) => <li key={index}>{c}</li>);
+  const list = <ul>{items}</ul>;
+  return [<h6>Format:</h6>, list];
 };
