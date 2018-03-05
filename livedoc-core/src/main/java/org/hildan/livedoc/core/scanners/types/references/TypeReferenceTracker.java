fb0c09dc77228a87109b119af1c66cc97fde7330
Joffrey BION <joffrey.bion@gmail.com>
2018-03-06 00:18:23 +0100

    Remove unused interface TypeReferenceTracker

--- livedoc-core/src/main/java/org/hildan/livedoc/core/scanners/types/references/TypeReferenceTracker.java
+++
@@ -1,8 +0,0 @@
-package org.hildan.livedoc.core.scanners.types.references;
-
-import java.util.Collection;
-
-public interface TypeReferenceTracker {
-
-    Collection<Class<?>> getReferencedTypes();
-}
