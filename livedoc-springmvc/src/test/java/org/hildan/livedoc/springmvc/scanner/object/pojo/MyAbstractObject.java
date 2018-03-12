940cffaf54cc9ef6d018b16f24063daa6b232b16
Joffrey BION <joffrey.bion@gmail.com>
2018-03-12 22:33:55 +0100

    Remove unused test classes

--- livedoc-springmvc/src/test/java/org/hildan/livedoc/springmvc/scanner/object/pojo/MyAbstractObject.java
+++
@@ -1,9 +0,0 @@
-package org.hildan.livedoc.springmvc.scanner.object.pojo;
-
-public abstract class MyAbstractObject {
-
-    private Integer id;
-
-    private String name;
-
-}
--- livedoc-springmvc/src/test/java/org/hildan/livedoc/springmvc/scanner/object/pojo/MyObject.java
+++
@@ -1,7 +0,0 @@
-package org.hildan.livedoc.springmvc.scanner.object.pojo;
-
-public class MyObject extends MyAbstractObject {
-
-    private Long age;
-
-}
