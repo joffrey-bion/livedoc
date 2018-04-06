eb363cb6b430e97d0d057bf06fb719af87293dbd
Joffrey Bion <joffrey.bion@amadeus.com>
2018-04-06 19:14:06 +0200

    Add global documentation to present the demo

--- sample-app/src/main/java/org/example/shelf/documentation/global/GlobalDocumentation.java
+++ sample-app/src/main/java/org/example/shelf/documentation/global/GlobalDocumentation.java
@@ -4,13 +4,7 @@ import org.hildan.livedoc.core.annotations.global.ApiGlobal;
 import org.hildan.livedoc.core.annotations.global.ApiGlobalSection;
 
 @ApiGlobal(sections = {
-        @ApiGlobalSection(title = "HTTP Verbs",
-                paragraphs = {
-                        "HTTP Verbs paragraph 1",
-                        "HTTP Verbs paragraph 2",
-                        "/livedocfile:/doc/global-verbs.html",
-                        "HTTP Verbs paragraph 3"
-                }),
+        @ApiGlobalSection(title = "What is this?", paragraphs = {"/livedocfile:/doc/global-top.html"}),
         @ApiGlobalSection(title = "Authentication", paragraphs = {"/livedocfile:/doc/global-authentication.html"}),
         @ApiGlobalSection(title = "Headers", paragraphs = {"/livedocfile:/doc/global-headers.html"}),
         @ApiGlobalSection(title = "Status codes", paragraphs = {"/livedocfile:/doc/global-responsestatuscodes.html"})
