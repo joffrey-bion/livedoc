7c737f82835b4a78155941dd13124c98e5fa62b1
Joffrey Bion <joffrey.bion@amadeus.com>
2018-04-06 15:00:42 +0200

    Fix javascript access in spring security

--- sample-app/src/main/java/org/example/shelf/auth/WebSecurityConfig.java
+++ sample-app/src/main/java/org/example/shelf/auth/WebSecurityConfig.java
@@ -14,6 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .antMatchers("/secure/**", "/users/**").hasRole("USER")
+            .anyRequest().permitAll()
             .and()
             .httpBasic();
     }
